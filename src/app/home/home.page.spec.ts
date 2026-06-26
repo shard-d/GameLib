4ad959a273455fb6385496664dc3560ce958ce55
shard-d <sreckovicd15@gmail.com>
2026-06-26 18:07:47 +0200

    initial commit

---
+++ src/app/home/home.page.spec.ts
@@ -0,0 +1,20 @@
+/// <reference types="jasmine" />
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomePage } from './home.page';
+
+describe('HomePage', () => {
+  let component: HomePage;
+  let fixture: ComponentFixture<HomePage>;
+
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(HomePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton
+  ]
+})
+export class LoginPage {
+  username = '';
+  password = '';
+
+  login() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
+}
